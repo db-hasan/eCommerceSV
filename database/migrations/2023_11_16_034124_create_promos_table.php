b773f2f42f302339cbe1bb96555fff30f1f46cd9
db-hasan <infoalihasanbd@gmail.com>
2023-11-16 09:45:19 +0600

    updt

---
+++ database/migrations/2023_11_16_034124_create_promos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promos', function (Blueprint $table) {
+            $table->id('promo_id');
+            $table->string('promo_name',50)->nullable();
+            $table->string('promo_code',50)->nullable();
+            $table->string('promo_amount',50)->nullable();
+            $table->integer('promo_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promos');
+    }
+};
